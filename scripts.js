04bfde3344c8982ae35228a0b41df87cc0218cb2
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2025-03-17 14:09:15 +0100

    wewe

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "450px"
+function openNav() { //Function
+    document.getElementById("mySidenav").style.width = "450px" //Changes css width of mySidenav to 450px
 }
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById("mySidenav").style.width = "0" //Changes css width of mySidenav to 0
 }
 
 function changeColor(){
-    document.body.style.backgroundColor = "red";
+    addingColor = document.getElementById("changeBcColor").value //Get the value of the input box
+    if (CSS.supports("color", addingColor) == true){ //Check if input value is supported by css color
+        document.body.style.backgroundColor = farge; //If true change to the color
+    }
+    else{
+        alert("Not a valid color") //If input not supported by css.
+    }
+}
+
+function addText(){
+    addingText = document.getElementById("addTextInput").value
+    if (addingText == ""){
+        alert("Invalid, no input given")
+    }
+    else{
+        let li = document.createElement("li")
+        li.textContent = addingText
+        document.getElementById("textList").appendChild(li)
+    }
 }
