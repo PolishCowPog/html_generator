df68d331f1f488a0da81052aa846d6200e9920c1
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2025-03-17 11:45:59 +0100

    basics

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px"
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"
+}
+
+function changeColor(){
+    document.body.style.backgroundColor = "red";
+}
